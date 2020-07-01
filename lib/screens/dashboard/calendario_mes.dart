a284b3be4ae305f0882aa11b64830689483ad86d
kelvingarcia <kelvinrgarcia98@gmail.com>
2020-07-01 11:06:18 -0300

    construção do calendário

--- lib/screens/dashboard/calendario_mes.dart
+++ lib/screens/dashboard/calendario_mes.dart
@@ -1,3 +1,4 @@
+import 'package:date_utils/date_utils.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:month_picker_dialog/month_picker_dialog.dart';
@@ -11,14 +12,46 @@ class _CalendarioMesState extends State<CalendarioMes> {
   DateTime mesSelecionado = DateTime.now();
   final DateFormat dateFormat = DateFormat(DateFormat.MONTH, 'pt_BR');
   String data;
+  DateTime ultimoDiaDoMes;
+  DateTime primeiroDia;
+  int numeroDias;
+  bool primeiro;
+  DateTime iteracao;
 
   @override
   void initState() {
+    posicaoMes(mesSelecionado);
     super.initState();
+  }
+
+  void posicaoMes(DateTime mesNovo) {
+    setState(() {
+      mesSelecionado = mesNovo;
+      primeiroDia = Utils.firstDayOfMonth(mesNovo);
+      ultimoDiaDoMes = Utils.lastDayOfMonth(mesNovo);
+      if (ultimoDiaDoMes.day == 28 && primeiroDia.weekday == 7) {
+        numeroDias = 28;
+      } else {
+        if((primeiroDia.weekday > 3 && ultimoDiaDoMes.day == 31) || (primeiroDia.weekday == 6 && ultimoDiaDoMes.day == 30)){
+          numeroDias = 42;
+        } else {
+          numeroDias = 35;
+        }
+      }
+    });
   }
 
   @override
   Widget build(BuildContext context) {
+    primeiro = false;
+    iteracao = primeiroDia;
+    if (primeiroDia.weekday < 7) {
+      for (int i = primeiroDia.weekday; i >= 0; i--) {
+        iteracao = DateTime(iteracao.year, iteracao.month, iteracao.day-1);
+      }
+    } else {
+      iteracao = DateTime(iteracao.year, iteracao.month, iteracao.day-1);
+    }
     return SizedBox(
       height: MediaQuery.of(context).size.height,
       width: MediaQuery.of(context).size.width,
@@ -31,9 +64,7 @@ class _CalendarioMesState extends State<CalendarioMes> {
               onTap: () async {
                 var novoMes = await showMonthPicker(
                     context: context, initialDate: DateTime.now());
-                setState(() {
-                  mesSelecionado = novoMes;
-                });
+                posicaoMes(novoMes);
               },
               child: Container(
                 height: MediaQuery.of(context).size.height * 0.07,
@@ -107,20 +138,36 @@ class _CalendarioMesState extends State<CalendarioMes> {
           Expanded(
             child: GridView.builder(
               padding: EdgeInsets.only(left: 16.0, right: 16.0),
-              itemCount: 31,
+              itemCount: numeroDias,
               gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                   crossAxisCount: 7,
                   crossAxisSpacing: 2.0,
                   mainAxisSpacing: 2.0),
               itemBuilder: (BuildContext context, int index) {
+                Color color;
+                int diaValidacao;
+                if(index == 0){
+                  diaValidacao = 7;
+                } else {
+                  diaValidacao = index;
+                }
+                if ((diaValidacao == primeiroDia.weekday || primeiro) &&
+                    iteracao.isBefore(ultimoDiaDoMes)) {
+                  color = Colors.white;
+                  primeiro = true;
+                } else {
+                  color = Colors.black;
+                  primeiro = false;
+                }
+                iteracao = DateTime(iteracao.year, iteracao.month, iteracao.day+1);
                 return Container(
                   height: 10.0,
                   width: 10.0,
                   child: Center(
                     child: Text(
-                      (index + 1).toString(),
+                      iteracao.day.toString(),
                       style: TextStyle(
-                        color: Colors.white,
+                        color: color,
                       ),
                     ),
                   ),
